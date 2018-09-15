1cf843f571aa3350e2a830386b1e0690dc26a3c3
masa <te09115t@yahoo.co.jp>
2018-09-15 20:33:15 +0900

    send data from client to server

--- app1.py
+++ app1.py
@@ -1,18 +1,55 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import model
 
 app = Flask(__name__)
 
+#Land top page
 @app.route("/")
-def login():
+def root():
     return render_template("login.html")
 
+#Sign in button, save button
 @app.route("/todo", methods = ["GET","POST"])
 def todo():
     if request.method == "POST":
-        return render_template("todo.html")
+        print("start_name&pass")
+        if(request.form["user_mail"],request.form["user_pass"]):
+            print("start_if")
+            user_mail = request.form["user_mail"]
+            user_pass = request.form["user_pass"]
+            print("user_mail = "+str(user_mail))
+            print("user_pass = "+str(user_pass))
+            print("end_if")
 
+        elif(request.form["title"]):
+            title = request.form["title"]
+            print(title)
+
+        """
+        priority = request.form["priority"]
+        deadline_date = request.form["deadline_date"]
+        deadline_time = request.form["deadline_time"]
+        time_req = request.form["time_req"]
+
+        print(priority)
+        print(deadline_date)
+        print(deadline_time)
+        print(time_req)
+        print("end_entry")
+        """
+        print("end_name&pass")
+        return render_template("todo.html")
     else:
         return redirect(url_for("login"))
+
+#Sign out button
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run()
